ee35932f45a3fd39f792c11e3bb05ddb0cc2f245
abmet <abmetko.a@gmail.com>
2020-11-21 00:48:55 +0300

    Test.java

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -1,9 +1,10 @@
 package com;
 
+/** mvn compile exec:exec -Darg1=Hello -Darg2=world! **/
+
 public class Test {
 
     public static void main(String[] args) {
         System.out.println(args[0] + ", " + args[1]);
     }
 }
-/* mvn clean compile exec:exec -Darg1=Hello -Darg2=world! */
